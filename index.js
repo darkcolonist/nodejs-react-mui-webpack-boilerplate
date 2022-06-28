37740269a36d60acb93a52cb9c8058170fbe37e1
cris <cris@nms.ph>
2022-06-28 13:26:59 +0800

    #1 setup development environment route catch-all to public/index.html make sure that all known requests are before `app.get('/*', (req, res) => {` line

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
+/**
+ * final catch-all route to index.html defined last
+ * this is so that react will handle all front-ends
+ */
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
